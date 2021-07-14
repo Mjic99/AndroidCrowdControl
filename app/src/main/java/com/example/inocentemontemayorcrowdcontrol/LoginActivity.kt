08b2e9aaa50d12f7d982360b27d801c59ba60a63
Fabrizio Montemayor <20170994@aloe.ulima.edu.pe>
2021-07-13 22:53:00 -0500

    LoginActivity y RegisterActivity para un tipo de usuario.

---
+++ app/src/main/java/com/example/inocentemontemayorcrowdcontrol/LoginActivity.kt
@@ -0,0 +1,77 @@
+package com.example.inocentemontemayorcrowdcontrol
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatButton
+import com.google.firebase.auth.FirebaseAuth
+import org.w3c.dom.Text
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Setup
+        setup()
+    }
+
+    private fun setup() {
+        val loginButton = findViewById<AppCompatButton>(R.id.loginButton)
+        val loginEmailET = findViewById<EditText>(R.id.loginEmailET)
+        val loginPasswordET = findViewById<EditText>(R.id.loginPasswordET)
+        val clientTV = findViewById<TextView>(R.id.clientTV)
+        val companyTV = findViewById<TextView>(R.id.companyTV)
+        title = "Autenticación"
+
+        loginButton!!.setOnClickListener {
+            if (loginEmailET!!.text.isNotEmpty() && loginPasswordET!!.text.isNotEmpty()) {
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(loginEmailET.text.toString(), loginPasswordET.text.toString()).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showClientActivity()
+                        } else {
+                            showAlert()
+                        }
+                    }
+            }
+        }
+
+        clientTV.setOnClickListener{
+            val registerIntent = Intent(this, RegisterActivity::class.java).apply {
+                    putExtra("userType", "client")
+            }
+            startActivity(registerIntent)
+        }
+        companyTV.setOnClickListener{
+            val registerIntent = Intent(this, RegisterActivity::class.java).apply {
+                putExtra("userType", "company")
+            }
+            startActivity(registerIntent)
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Error de Autenticación")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showClientActivity() {
+        val clientIntent = Intent(this, MapsActivity::class.java).apply {
+        }
+        startActivity(clientIntent)
+    }
+
+    private fun showCompanyActivity() {
+        // TODO: Agregar cambio a actividad de empresa
+    }
+
+}
---
+++ app/src/main/java/com/example/inocentemontemayorcrowdcontrol/RegisterActivity.kt
@@ -0,0 +1,60 @@
+package com.example.inocentemontemayorcrowdcontrol
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val bundle = intent.extras
+        val userType = bundle!!.getString("userType")
+        // Setup
+        setup(userType!!)
+    }
+
+    private fun setup(userType: String) {
+        val registerButton = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.registerButton)
+        val registerEmailET = findViewById<EditText>(R.id.registerEmailET)
+        val registerPasswordET = findViewById<EditText>(R.id.registerPasswordET)
+        title = "Registro"
+        registerButton!!.setOnClickListener {
+            if (registerEmailET!!.text.isNotEmpty() && registerPasswordET!!.text.isNotEmpty()) {
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(registerEmailET.text.toString(), registerPasswordET.text.toString()).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            // Obtenemos el ID del usuario recién creado
+                            val userId : String = it.getResult()!!.user!!.uid
+                            showClientActivity()
+                        } else {
+                            showAlert()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Error de Registro")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showClientActivity() {
+        val clientIntent = Intent(this, MapsActivity::class.java).apply {
+        }
+        startActivity(clientIntent)
+    }
+
+    private fun showCompanyActivity() {
+        // TODO: Agregar cambio a actividad de empresa
+    }
+}
